183b6480133fde004af0cc096d7f8da3660207dc
peterSanyo <peter.sanyo@code.berlin>
2023-03-10 13:10:33 +0100

    displaying objects on landing page, including donwload link for description-file, gitignore update

--- 02_assessment/app.py
+++ 02_assessment/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, send_file
 app = Flask(__name__)
 app.config.from_object("config")
 
@@ -19,7 +19,7 @@ pieces = {
     "image": "IMAGE OF ELEGANT LUXURY-CAR",
     "quote": ' "a state-of-the-art experience [...] by the forces of nature." ',
     "quoted":"Iris van Herpen",
-    "offerer":"Rolls Royce X iris van Herpen",
+    "offerer":"Rolls Royce X Iris van Herpen",
     "dimensions": "571x214x157 cm",
     "materials": "Leather, Fabric, Steel",
     "category":"Vehicle",
@@ -52,10 +52,57 @@ pieces = {
 # Landin-page, overview and downloadable material 
 @app.route("/gallery")
 def landing():
-  return render_template("01_landing.html")
+  pieces = {
+  "era300" : {
+    "name": "Era 300",
+    "image": "IMAGE OF MINIMALIST SPEAKER",
+    "quote": ' "The most suffisticated product Sonos has built" ',
+    "quoted":"Dana Krieger, head of Sonos",
+    "offerer":"Sonos",
+    "dimensions": "30x20x10 cm",
+    "materials": "hard case plastic and wood",
+    "category":"Spatial home Audio",
+    "price": " 399$"
+    },
+    "phantom_syntopia" : {
+    "name": "Phantom Syntopia",
+    "image": "IMAGE OF ELEGANT LUXURY-CAR",
+    "quote": ' "a state-of-the-art experience [...] by the forces of nature." ',
+    "quoted":"Iris van Herpen",
+    "offerer":"Rolls Royce X Iris van Herpen",
+    "dimensions": "571x214x157 cm",
+    "materials": "Leather, Fabric, Steel",
+    "category":"Vehicle",
+    "price": "-"
+    },
+    "white_fromme" : {
+    "name": "White Fromme Collection",
+    "image": "IMAGE OF MINIMALISTIC SET OF INTERIOUR DESIGN CLASSIS",
+    "quote": ' "This design was inspired by the personal story of Tom Chung." ',
+    "quoted":"Petite Friture, French Design Brand",
+    "offerer":"Petite Friture X Tom Chang",
+    "dimensions": "-",
+    "materials": "Hardwearing Aluminium",
+    "category":"Exclusive Furniture",
+    "price": "-"
+    },
+    "color_palette" : {
+    "name": "2023 Color Palette",
+    "image": "IMAGE OF TRANSLUCENT GLASSSTRIPES OF A PASTELL COLOUR PALETTE",
+    "quote": ' "This collection allowed us to be more introspective about the meaning of colour in our lives." ',
+    "quoted":"Ryan Smith, chief creative of 3form",
+    "offerer":"3form",
+    "dimensions": "-",
+    "materials": "Varia, Chroma and Glass",
+    "category":"Modular Interiour Design ",
+    "price": "-"
+    },
+}
+
+  return render_template("01_landing.html", pieces=pieces)
 
 # Exhibit 1 
-@app.route("/exhibit1")
+@app.route("/exhibit1", methods=["GET", "WRITE"])
 def product1():
   name="Paul"
   object=pieces["era300"]
@@ -125,6 +172,11 @@ def product4():
   category=object["category"],
   price=object["price"],
   )
+
+# download 
+@app.route("/downloads")
+def exhibition():
+  return send_file("static/downloads/exhibition.txt", as_attachment=True)
 
 if __name__ == "__main__":
   app.run()
